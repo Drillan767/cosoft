179f436cca500c245ef3877f6952aad75ccb1e12
Drillan767 <jd.levarato@gmail.com>
2026-02-07 11:54:54 +0100

    WIP

--- internal/slackbot/models.go
+++ internal/slackbot/models.go
@@ -1,8 +1,8 @@
 package slackbot
 
 type Request struct {
-	UserId      string `json:"user_id"`
-	Command     string `json:"command"`
-	Text        string `json:"text"`
-	ResponseUrl string `json:"response_url"`
+	UserId      string
+	Command     string
+	Text        string
+	ResponseUrl string
 }
