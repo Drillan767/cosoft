676f8abaeeabb070041125e974d8971aa34df0da
Drillan767 <jd.levarato@gmail.com>
2026-02-07 11:54:46 +0100

    Database handling is done

--- slack/main.go
+++ slack/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "cosoft-cli/internal/slackbot"
+import (
+	"cosoft-cli/internal/slackbot"
+	"cosoft-cli/internal/storage"
+	"log"
+)
 
 func main() {
+	store, err := storage.NewStore("./slack/database.db")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Ensure database exists and is migrated
+	err = store.SetupDatabase()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	bot := slackbot.NewBot()
 	bot.StartServer()
 }
